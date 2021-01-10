2a1a71c360a3d5f0ade4972e3fe6585abacba754
faical23 <65980103+faical23@users.noreply.github.com>
2021-01-10 16:36:47 +0100

    J

--- bref.c
+++ bref.c
@@ -11,6 +11,8 @@
         char starting[N];
         int vote[v]; /// arrays of stocking les vote
         int index = 0;  /// variable stocky every vote a chaque loops.
+        float result[N]; /// stockage OF EVERY PERCENTAGE OF PRESEDENT
+
 
         //// partie add les presedent
         
@@ -31,7 +33,7 @@
       
         /////: partie de premier vote
 
-            void parte_vote()
+            void parte_vote_1()
             {
 
                 for(int i = 0 ; i < v; i++)
@@ -68,7 +70,6 @@
 
                 /// show les vote pour percentage % of every presedent
 
-                float result[N];
                  printf("\nles perentage vote a chaque presedent : \n");
                 for(int i = 0 ; i < N; i++)
                 {
@@ -81,17 +82,17 @@
                     /// show if all vote its egale or not
 
                     printf("\nshow if les vote is egale or nn \n");
-                    int test_egal=0;                    
+                    int test_egal=0;  /// variable incrument if the vote is egal                  
                     for(int i = 0 ; i <N ; i++)
                     {
                         int next_vote = i+1;
                         if(starting[i] == starting[next_vote])
                         {
-                            test_egal++; ///chaque egal we incrument this value
+                            test_egal++; ///every egal we incrument this value
                         }
                         
                     }
-                    if( test_egal == N-1)
+                    if( test_egal == N-1)  // N-1 == cuz test_egal he not arrive fin the N
                     {
                         //// her we repete the vote because all presedent has the same vote
                         printf("is egale cuz test_egal = %i  need to repete the vote \n",test_egal);
@@ -99,14 +100,12 @@
                         {
                             starting[i] = 0;
                         }
-                        parte_vote();
+                        parte_vote_1();
                     }
                     else{
                         printf("is not egale cuz  test_egal= %i u should go to the next vote ",test_egal);
                         /// her we remove the presedent he has less vote and started new vote
                         /// we should desincrument N
-                        //// script show index of the presedent we should removeted in the arrays
-                        //float mn;
                         int index_presedent_delete = 0;
                        // mn = result[0]; /// mn le prmier value in rhis arrays
                         for(int i=1; i < N; i++)
@@ -114,97 +113,25 @@
 
                             if(result[i] < 15) /// we SHOW if the result of vote every presedent is less than 15%
                             {
-                                //mn = result[i];
                                 index_presedent_delete = i; // the index he incrument
                                 printf("\n");
                                 printf("presedent has a vote less than 15%% is %s and the index of this presedent  is %i " , presedent[index_presedent_delete],index_presedent_delete);
                                 printf("\n");
+                                        for (int i = index_presedent_delete ; i < N ; i++)
+                                        presedent[i][20] = presedent[i+1][20];
+                                        N--;
                             }
+                            printf("%s\n" ,  presedent[i]);
+
                         }
 
                     }
             }
            
-            parte_vote();
+            parte_vote_1();
 
 
 
-
-
-            
-
-
-
-
-
-
-                    
- /*     
-        /// show les vote
-
-        int stockage_number_vote[N];  // arrays for stockage total of vote evvery presedent
-        int n = 1; /// pour strted from 1 if n++ because we have a problem if started with 0
-        int size = v; // pour don't touch v
-        printf("this is all vote aviable : \n");
-        for(int i = 0 ; i < size ; i++)
-        {
-            for(int j = i+1 ; j < size ; j++)
-            {
-                if(vote[i] == vote[j])
-                {
-                    // Delete the current duplicate element 
-                    for(int k=j ; k < size ; k++)
-                    {
-                        vote[k] = vote[k + 1];
-                    }
-
-                    // Decrement size after removing duplicate element 
-                    size--;
-
-                    // If shifting of elements occur then don't increment j 
-                    j--;
-                    n++;
-                }
-            }
-            printf("%i -> %s \n", n , presedent[i]);
-            stockage_number_vote[i] = n; // stockage number of vote in arrays
-            n = 1;
-        }
-
-            for(int i = 0 ; i <N ;i++)
-            {
-                printf("- %i -",stockage_number_vote[i]); // show all hvote we have
-            }
-            printf("\n");
-
-        for(int i = 0 ; i <N ;i++)
-        {
-            printf("%s number vote : %i \n",presedent[i] , stockage_number_vote[i]); // show all presedent with her votes
-        }
-
-        //// script show index of the presedent we should removeted in the arrays
-
-            int mn;
-            int index = 0;
-            mn = stockage_number_vote[0]; /// mn le prmier value in rhis arrays
-
-            for(int i=1; i < N; i++)
-            {
-
-                if(stockage_number_vote[i] < mn) /// we do comparaison between this arrays and show the minnumun 
-                {
-                    mn = stockage_number_vote[i];
-                    index = i; // the index he incrument
-                }
-            }
-            printf("\n");
-            printf("Minimum vote presedent  is %i and the presedent weshould removeted is  : %s", mn , presedent[index]);
-            printf("\n");
-
-
-*/
-
-                 
     }
 
 

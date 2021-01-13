298126a9d9efcc06af347a5f541f6f05c26bf951
faical23 <65980103+faical23@users.noreply.github.com>
2021-01-13 21:20:02 +0100

    modification

--- tempCodeRunnerFile.c
+++
@@ -1,3 +0,0 @@
- of this presedent  is %i " , presedent[index_presedent_delete],index_presedent_delete);
-                                printf("\n");
-                                delete_presedent[z] = index_presedent_delete; // stockage index
